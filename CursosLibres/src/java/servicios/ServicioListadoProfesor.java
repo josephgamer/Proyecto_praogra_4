a7ce31fb511dbbb6dadfa81efc8cb206e40bd53d
GabGu <45882851+GabGu@users.noreply.github.com>
2021-04-28 16:13:25 -0600

    Import

--- CursosLibres/src/java/servicios/ServicioListadoProfesor.java
+++ CursosLibres/src/java/servicios/ServicioListadoProfesor.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelo.Curso;
 import modelo.beans.Profesor;
+import modelo.beans.ConjuntoProfesor;
 
 /**
  *
