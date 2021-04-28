1237749be09308b1f3712a3c2bfbe58ae817d1a9
YENDRI <Yendri2798>
2021-04-28 16:50:23 -0600

    Listado profesor

--- CursosLibres/src/java/servicios/ServicioListadoProfesor.java
+++ CursosLibres/src/java/servicios/ServicioListadoProfesor.java
@@ -6,14 +6,13 @@
 package servicios;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.text.ParseException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modelo.Curso;
 import modelo.beans.Profesor;
 import modelo.beans.ConjuntoProfesor;
 
@@ -25,51 +24,51 @@ import modelo.beans.ConjuntoProfesor;
 @WebServlet(name = "ServicioListadoProfesor", urlPatterns = {"/ServicioListadoProfesor"})
 public class ServicioListadoProfesor extends HttpServlet {
 
-//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-//
-//        System.out.println("Servlet ServicioListadoProfesor..");
-//
-//        try {
-//            Profesor p = new Profesor(
-//                    Integer.parseInt(request.getParameter("id")),
-//                    request.getParameter("apellido1"),
-//                    request.getParameter("apellido2"),
-//                    request.getParameter("nombre"),
-//                    request.getParameter("correo"),
-//                    request.getParameter("telefono"));
-//      
-//            ConjuntoProfesor personas
-//                    = (ConjuntoPersonas) getServletContext().getAttribute("cursos");
-//            personas.agregar(c);
-//            System.out.println(personas);
-//
-//            response.sendRedirect("historial.jsp");
-//
-//        } catch (IOException | NumberFormatException | SQLException | ParseException ex) {
-//            System.err.printf("Excepción: '%s'%n", ex.getMessage());
-//            response.sendRedirect("error.jsp");
-//        }
-//
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);
-//    }
-//
-//    @Override
-//    public String getServletInfo() {
-//        return "ServicioHistorial";
-//    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        System.out.println("Servlet ServicioListadoProfesor..");
+
+        try {
+            Profesor p = new Profesor(
+                    Integer.parseInt(request.getParameter("id")),
+                    request.getParameter("apellido1"),
+                    request.getParameter("apellido2"),
+                    request.getParameter("nombre"),
+                    request.getParameter("correo"),
+                    request.getParameter("telefono"));
+      
+            ConjuntoProfesor personas
+                    = (ConjuntoProfesor) getServletContext().getAttribute("profesores");
+            personas.agregar(p);
+            System.out.println(personas);
+
+            response.sendRedirect("listadoProfesor.jsp");
+
+        } catch (IOException | NumberFormatException | SQLException  ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+            response.sendRedirect("error.jsp");
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "ServicioListadoProfesor";
+    }
 
 }
 
---
+++ CursosLibres/web/error.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : error
+    Created on : 28 abr. 2021, 16:43:27
+    Author     : YENDRI
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Error</title>
+        <meta charset="UTF-8">
+        <link href="css/default.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <h1>Listado de personas</h1>
+            </header>
+            <nav>
+                <p>
+                    <a href="listadoProfesor.jsp">Ir al listado de profesor</a>
+                </p>
+            </nav>
+            <div id="contents">
+                <p class="error">
+                    Ocurrió un error al procesar los datos del formulario.
+                </p>
+            </div>
+            <footer></footer>
+        </div>
+    </body>
+</html>
