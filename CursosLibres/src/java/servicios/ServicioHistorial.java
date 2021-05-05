bf7426c7bc04b24f35fec5c10d8802a85ef6b8f1
Greivin-12 <61101300+Greivin-12@users.noreply.github.com>
2021-05-05 14:13:13 -0600

    PDF

--- CursosLibres/src/java/servicios/ServicioHistorial.java
+++ CursosLibres/src/java/servicios/ServicioHistorial.java
@@ -5,6 +5,13 @@
  */
 package servicios;
 
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -12,11 +19,18 @@ import java.io.PrintWriter;
 //import javax.servlet.http.HttpServlet;
 //import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.http.HttpServletResponse;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.awt.Font;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import modelo.beans.ConjuntoCurso;
+import modelo.beans.ConjuntoGrupo;
+import modelo.beans.Curso;
+import modelo.beans.Grupo;
+import modelo.beans.Matricula;
 
 /**
  *
@@ -41,13 +55,62 @@ public class ServicioHistorial extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ServicioHistorial</title>");            
+            out.println("<title>Servlet ServicioHistorial</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ServicioHistorial at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
+    }
+
+    public static void crearPDF(Matricula matricula) throws FileNotFoundException, DocumentException {
+        Document documento = new Document();
+        FileOutputStream ficheroPDF = new FileOutputStream("Matricula.pdf");
+
+        PdfWriter.getInstance(documento, ficheroPDF);
+
+        documento.open();
+        Paragraph titulo = new Paragraph("Historial de cursos \n\n",
+                FontFactory.getFont("arial", 22, Font.BOLD, BaseColor.BLUE)
+        );
+        
+        documento.add(titulo);
+        
+        PdfPTable tabla = new PdfPTable(9);
+        tabla.addCell("Estudiante_id");
+        tabla.addCell("Num_grupo");
+        tabla.addCell("Descripcion_area");
+        tabla.addCell("Descricion_curso");
+        tabla.addCell("Nombre_profesor");
+        tabla.addCell("Apellido_1");
+        tabla.addCell("Apellido_2");
+        tabla.addCell("Nota");
+        tabla.addCell("Estado");
+        
+        tabla.addCell(String.valueOf(matricula.getEstudiante_id()));
+        tabla.addCell(String.valueOf(matricula.getGrupo_num()));
+        tabla.addCell(obtenerCurso(matricula.getCurso_id()).getAreaTematica_id().getTematica_descrip());
+        tabla.addCell(obtenerCurso(matricula.getCurso_id()).getCurso_descrip());
+        tabla.addCell(obtenerGrupo(matricula.getGrupo_num()).getProfesor_id().getNombre());
+        tabla.addCell(obtenerGrupo(matricula.getGrupo_num()).getProfesor_id().getApellido1());
+        tabla.addCell(obtenerGrupo(matricula.getGrupo_num()).getProfesor_id().getApellido2());
+        tabla.addCell(String.valueOf(matricula.getNota()));
+        tabla.addCell(matricula.getEstado_id().getEst_descripcion());
+        
+        documento.add(tabla);
+        
+        documento.close();
+    }
+    
+    public static Curso obtenerCurso(int id_curso){
+        ConjuntoCurso obj = new ConjuntoCurso();
+        return obj.obtenerCurso(id_curso);
+    }
+    
+    public static Grupo obtenerGrupo(int id_grupo){
+        ConjuntoGrupo obj = new ConjuntoGrupo();
+        return obj.getGrupo(id_grupo);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
